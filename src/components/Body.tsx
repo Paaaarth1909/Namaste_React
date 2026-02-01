c7ff94d7c588d846f378b88b20f96e78ccaa36aa
Parthsaarthie sharma <toshit113@gmail.com>
2026-02-01 23:34:29 +0530

    ep 6 done

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -4,71 +4,70 @@ import resList from "../utils/mockData";
 import type { Restaurant } from "../utils/Types";
 import Shimmer from "./shimmer";
 
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState<Restaurant[]>([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState<Restaurant[]>(
+    [],
+  );
+  const [searchText, setSearchText] = useState("");
 
-const Body = () => {const [listOfRestaurants, setListOfRestaurants] = useState<Restaurant[]>([]);
-const [filteredRestaurants, setFilteredRestaurants] = useState<Restaurant[]>([]);
-const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-useEffect(() => {
-  fetchData();
-}, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://namastedev.com/api/v1/listRestaurants",
+      );
 
-const fetchData = async () => {
-  try {
-    const response = await fetch(
-      "https://namastedev.com/api/v1/listRestaurants"
-    );
+      const json = await response.json();
 
-    const json = await response.json();
+      const restaurants =
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants;
 
-    const restaurants =
-      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+      setListOfRestaurants(restaurants || []);
+      setFilteredRestaurants(restaurants || []);
+    } catch (error) {
+      setListOfRestaurants(resList);
+      setFilteredRestaurants(resList);
+    }
+  };
 
-    setListOfRestaurants(restaurants || []);
-    setFilteredRestaurants(restaurants || []);
-  } catch (error) {
-
-    setListOfRestaurants(resList);
-    setFilteredRestaurants(resList);
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
   }
-};
-
-if (listOfRestaurants.length === 0) {
-  return <Shimmer />;
-}
-
 
   return (
     <div className="body">
       <div className="filter">
         <div className="search">
-  <input
-    className="search-box"
-    type="text"
-    value={searchText}
-    onChange={(e) => setSearchText(e.target.value)}
-  />
-  <button
-    onClick={() => {
-      const filtered = listOfRestaurants.filter((res) =>
-        res.data.name
-          .toLowerCase()
-          .includes(searchText.toLowerCase())
-      );
-      setFilteredRestaurants(filtered);
-    }}
-  >
-    Search
-  </button>
-</div>
+          <input
+            className="search-box"
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              const filtered = listOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase()),
+              );
+              setFilteredRestaurants(filtered);
+            }}
+          >
+            Search
+          </button>
+        </div>
 
         <button
           className="filter-btn"
           onClick={() => {
             const filtered = listOfRestaurants.filter(
-  (res) => parseFloat(res.data.avgRating) > 4
-);
-setFilteredRestaurants(filtered);
+              (res) => parseFloat(res.data.avgRating) > 4,
+            );
+            setFilteredRestaurants(filtered);
           }}
         >
           Top Rated Restaurants
@@ -76,11 +75,8 @@ setFilteredRestaurants(filtered);
       </div>
 
       <div className="res-container">
-        {filteredRestaurants.map((restaurant,info) => (
-          <RestaurantCard
-            key={restaurant.data.id}
-            resData={restaurant}
-          />
+        {filteredRestaurants.map((restaurant, info) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
     </div>
